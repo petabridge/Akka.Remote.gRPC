1ea561a33e399ba22308c2a1359f55f8d42e5747
Aaron Stannard <aaron@petabridge.com>
2022-02-21 12:04:11 -0600

    implementing the basics

--- src/Akka.Remote.gRPC/Class1.cs
+++ src/Akka.Remote.gRPC/Class1.cs
@@ -1,42 +1,74 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Event;
 using Akka.Remote.Transport;
 using Akka.Remote.Transport.gRPC;
 using Google.Protobuf;
 using Grpc.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Akka.Remote.gRPC
 {
-    internal class GrpcHandler
+    internal sealed class GrpcAssociationHandle : AssociationHandle
     {
-        
+        private readonly GrpcHandler _handler;
+
+        public GrpcAssociationHandle(Address localAddress, Address remoteAddress, GrpcHandler handler) : base(
+            localAddress, remoteAddress)
+        {
+            _handler = handler;
+        }
+
+        public override bool Write(ByteString payload)
+        {
+            return _handler.Write(payload);
+        }
+
+        public override void Disassociate()
+        {
+            // begin closing the channel
+#pragma warning disable CS4014
+            _handler.CloseAsync();
+#pragma warning restore CS4014
+        }
     }
-    
+
     /// <summary>
-    /// Outbound
+    /// Reusable abstraction shared between gRPC client and server connections.
     /// </summary>
-    internal sealed class GrpcClient
+    internal sealed class GrpcHandler : IDisposable, IEquatable<GrpcHandler>
     {
         private readonly IAsyncStreamReader<Payload> _requestStream;
         private readonly IServerStreamWriter<Payload> _responseStream;
-        
+
         // driven by the gRPC connection
         private readonly CancellationToken _grpcCancellationToken;
-        
+
         // managed by us
         private readonly CancellationTokenSource _internalCancellationToken;
 
         private IHandleEventListener _listener;
-        private TaskCompletionSource<Done> _readHandlerSet;
-        private TaskCompletionSource<Done> _shutdownTask;
+        private readonly TaskCompletionSource<Done> _readHandlerSet;
+        private readonly TaskCompletionSource<Done> _shutdownTask;
 
-        public GrpcClient(IAsyncStreamReader<Payload> requestStream, IServerStreamWriter<Payload> responseStream, CancellationToken grpcCancellationToken)
+        public GrpcHandler(IAsyncStreamReader<Payload> requestStream, IServerStreamWriter<Payload> responseStream,
+            Address remoteAddress, Address localAddress, CancellationToken grpcCancellationToken)
         {
             _requestStream = requestStream;
             _responseStream = responseStream;
             _grpcCancellationToken = grpcCancellationToken;
+            RemoteAddress = remoteAddress;
+            LocalAddress = localAddress;
             _internalCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(grpcCancellationToken);
             _readHandlerSet = new TaskCompletionSource<Done>(TaskCreationOptions.RunContinuationsAsynchronously);
             _shutdownTask = new TaskCompletionSource<Done>(TaskCreationOptions.RunContinuationsAsynchronously);
@@ -63,7 +95,7 @@ namespace Akka.Remote.gRPC
             if (!_internalCancellationToken.IsCancellationRequested)
             {
                 // don't await
-                _responseStream.WriteAsync(new Payload(){Message = message});
+                _responseStream.WriteAsync(new Payload() { Message = message });
                 return true;
             }
 
@@ -73,6 +105,10 @@ namespace Akka.Remote.gRPC
         public Task<Done> WhenTerminated => _shutdownTask.Task;
 
         public Task<Done> WhenReadOpen => _readHandlerSet.Task;
+        
+        public Address RemoteAddress { get; }
+        
+        public Address LocalAddress { get; }
 
         private async Task DoRead()
         {
@@ -83,17 +119,127 @@ namespace Akka.Remote.gRPC
                 _listener.Notify(new InboundPayload(read.Message));
             }
         }
+
+        public void Dispose()
+        {
+            _internalCancellationToken?.Cancel();
+            _internalCancellationToken?.Dispose();
+        }
+        
+        public bool Equals(GrpcHandler other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return RemoteAddress.Equals(other.RemoteAddress) && LocalAddress.Equals(other.LocalAddress);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is GrpcHandler other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RemoteAddress, LocalAddress);
+        }
+
+        public static bool operator ==(GrpcHandler left, GrpcHandler right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(GrpcHandler left, GrpcHandler right)
+        {
+            return !Equals(left, right);
+        }
     }
-    
+
+    public sealed class GrpcTransport : Akka.Remote.Transport.Transport
+    {
+        public GrpcTransport(ActorSystem system, Config config)
+        {
+            System = system;
+            Config = config;
+            Settings = GrpcTransportSettings.Create(config);
+            Log = Logging.GetLogger(System, GetType());
+            _associationListenerPromise = new TaskCompletionSource<IAssociationEventListener>();
+        }
+
+        private readonly TaskCompletionSource<IAssociationEventListener> _associationListenerPromise;
+        private readonly IWebHost _host;
+        
+        // all open connections
+        private readonly ConcurrentBag<GrpcHandler> _handlers = new ConcurrentBag<GrpcHandler>();
+
+        public ILoggingAdapter Log { get; }
+
+        public ActorSystem System { get; }
+
+        public GrpcTransportSettings Settings { get; }
+
+        public override async Task<(Address, TaskCompletionSource<IAssociationEventListener>)> Listen()
+        {
+            EndPoint listenAddress;
+            IPAddress ip;
+            if (IPAddress.TryParse(Settings.Hostname, out ip))
+                listenAddress = new IPEndPoint(ip, Settings.Port);
+            else
+                listenAddress = new DnsEndPoint(Settings.Hostname, Settings.Port);
+
+            try
+            {
+                var builder = WebApplication.CreateBuilder();
+                builder.WebHost.ConfigureKestrel(options =>
+                    {
+                        // bind to the specified address
+                        options.Listen(listenAddress, listenOptions =>
+                        {
+                            // todo: want HTTP1 to support platforms like Azure App Service,
+                            // but  need to lean on HTTP2 as the default for performance reasons.
+                            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                        });
+
+                        // todo - configure max frame size et al here
+                        // see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/options?view=aspnetcore-6.0
+                        // options.Limits
+                    })
+                    .ConfigureServices(services =>
+                    {
+                        services.AddGrpc(options =>
+                        {
+                            options.IgnoreUnknownServices = true;
+
+                            // TODO: this might be a better place to set frame size limits
+                            options.MaxReceiveMessageSize = null;
+                            options.MaxSendMessageSize = null;
+                        });
+
+                    });
+            }
+        }
+
+        public override bool IsResponsibleFor(Address remote)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<AssociationHandle> Associate(Address remoteAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<bool> Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     /// <summary>
     /// Inbound
     /// </summary>
-    internal sealed class GrpcTransport : AkkaRemote.AkkaRemoteBase
+    internal sealed class GrpcServerListener : AkkaRemote.AkkaRemoteBase
     {
-        private readonly Channel<ByteString> _writeBytes;
-        private readonly Channel<ByteString> _readBytes;
-
-        public GrpcTransport(Channel<ByteString> writeBytes, Channel<ByteString> readBytes)
+        public GrpcServerListener(Channel<ByteString> writeBytes, Channel<ByteString> readBytes)
         {
             _writeBytes = writeBytes;
             _readBytes = readBytes;
@@ -102,7 +248,8 @@ namespace Akka.Remote.gRPC
         /*
          * New server handle will need to be created each time here...
          */
-        public override async Task MessageEndpoint(IAsyncStreamReader<Payload> requestStream, IServerStreamWriter<Payload> responseStream, ServerCallContext context)
+        public override async Task MessageEndpoint(IAsyncStreamReader<Payload> requestStream,
+            IServerStreamWriter<Payload> responseStream, ServerCallContext context)
         {
             //context.GetHttpContext().Connection.
             async Task DoWrites()
---
+++ src/Akka.Remote.gRPC/GrpcTransportSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using Akka.Configuration;
+
+namespace Akka.Remote.gRPC;
+
+public sealed record GrpcTransportSettings
+{
+    public static GrpcTransportSettings Create(Config config)
+    {
+        if (config.IsNullOrEmpty())
+            throw ConfigurationException.NullOrEmptyConfig<GrpcTransportSettings>();
+
+        var host = config.GetString("hostname", null);
+        if (string.IsNullOrEmpty(host)) host = IPAddress.Any.ToString();
+        var publicHost = config.GetString("public-hostname", null);
+        var publicPort = config.GetInt("public-port", 0);
+
+        var connectTimeout = config.GetTimeSpan("connection-timeout", TimeSpan.FromSeconds(15));
+
+        return new GrpcTransportSettings()
+        {
+            ConnectTimeout = connectTimeout,
+            Hostname = host,
+            PublicHostname = !string.IsNullOrEmpty(publicHost) ? publicHost : host,
+            Port = config.GetInt("port", 2553),
+            PublicPort = publicPort > 0 ? publicPort : null
+        };
+    }
+
+    /// <summary>
+    /// Sets a connection timeout for all outbound connections
+    /// i.e. how long a connect may take until it is timed out.
+    /// </summary>
+    public TimeSpan ConnectTimeout { get; init; }
+
+    /// <summary>
+    /// The hostname or IP to bind the remoting to.
+    /// </summary>
+    public string Hostname { get; init; }
+
+    /// <summary>
+    /// If this value is set, this becomes the public address for the actor system on this
+    /// transport, which might be different than the physical ip address (hostname)
+    /// this is designed to make it easy to support private / public addressing schemes
+    /// </summary>
+    public string PublicHostname { get; init; }
+
+    /// <summary>
+    /// The default remote server port clients should connect to.
+    /// Default is 2552 (AKKA), use 0 if you want a random available port
+    /// This port needs to be unique for each actor system on the same machine.
+    /// </summary>
+    public int Port { get; init; }
+
+    /// <summary>
+    /// If this value is set, this becomes the public port for the actor system on this
+    /// transport, which might be different than the physical port
+    /// this is designed to make it easy to support private / public addressing schemes
+    /// </summary>
+    public int? PublicPort { get; init; }
+}
